d44d5985359634112a83e1d42344a298c81197b9
henrytwagner <htwags22@gmail.com>
2025-04-09 14:20:50 +0200

    Fix layout scrolling and add contact naem truncation

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -9,201 +9,201 @@ const ContactList = () => {
       <div className="contactlist-scrollable">
         <div className="characterheader">A</div>
         <div className="person">
-          <p>
+          <p className="personname">
             Mike <span className="lastname">Adams</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Sarah <span className="lastname">Allen</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Jake <span className="lastname">Armstrong</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Lisa <span className="lastname">Andrews</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Ben <span className="lastname">Avery</span>
           </p>
         </div>
 
         <div className="characterheader">B</div>
         <div className="person">
-          <p>
+          <p className="personname">
             Emily <span className="lastname">Baker</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Tom <span className="lastname">Barnes</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Ashley <span className="lastname">Barrett</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             John <span className="lastname">Bennett</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Anna <span className="lastname">Bradley</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Chris <span className="lastname">Brooks</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Rachel <span className="lastname">Brown</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Daniel <span className="lastname">Butler</span>
           </p>
         </div>
 
         <div className="characterheader">C</div>
         <div className="person">
-          <p>
+          <p className="personname">
             Kevin <span className="lastname">Caldwell</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Jessica <span className="lastname">Campbell</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Luke <span className="lastname">Carter</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Megan <span className="lastname">Chambers</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Alex <span className="lastname">Clark</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Nicole <span className="lastname">Cohen</span>
           </p>
         </div>
         <div className="characterheader">D</div>
         <div className="person">
-          <p>
+          <p className="personname">
             David <span className="lastname">Daniels</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Laura <span className="lastname">Davis</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Tyler <span className="lastname">Day</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Sophia <span className="lastname">Delaney</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Mark <span className="lastname">Dixon</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Amanda <span className="lastname">Donovan</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Paul <span className="lastname">Douglas</span>
           </p>
         </div>
 
         <div className="characterheader">E</div>
         <div className="person">
-          <p>
+          <p className="personname">
             Karen <span className="lastname">Edwards</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Michael <span className="lastname">Ellis</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Emily <span className="lastname">Emerson</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Brian <span className="lastname">Evans</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Jenna <span className="lastname">Elliott</span>
           </p>
         </div>
 
         <div className="characterheader">F</div>
         <div className="person">
-          <p>
+          <p className="personname">
             Tyler <span className="lastname">Ferguson</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Hailey <span className="lastname">Fisher</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Nathan <span className="lastname">Fleming</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Chloe <span className="lastname">Ford</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Isaac <span className="lastname">Foster</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Samantha <span className="lastname">Franklin</span>
           </p>
         </div>
         <div className="person">
-          <p>
+          <p className="personname">
             Derek <span className="lastname">Fuller</span>
           </p>
         </div>
