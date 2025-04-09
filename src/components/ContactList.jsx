99ba765d4c10ccf5671d39f31d6e664e081d8ee3
henrytwagner <htwags22@gmail.com>
2025-04-09 20:32:34 +0200

    Enable contact retreval from backend

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,104 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/contactlist.css';
 import SearchBar from './SearchBar';
-
-const contacts = [
-  // A
-  { first: 'Mike', last: 'Adams' },
-  { first: 'Sarah', last: 'Akers' },
-  { first: 'Liam', last: 'Albright' },
-  { first: 'Emma', last: 'Aldridge' },
-  { first: 'Noah', last: 'Alexander' },
-  { first: 'Olivia', last: 'Alford' },
-  { first: 'Elijah', last: 'Allen' },
-  { first: 'Ava', last: 'Allison' },
-  { first: 'James', last: 'Alston' },
-  { first: 'Sophia', last: 'Alvarez' },
-  // B
-  { first: 'Logan', last: 'Burger' },
-  { first: 'Blake', last: 'Bryant' },
-  { first: 'Zoe', last: 'Benson' },
-  { first: 'Miles', last: 'Baker' },
-  { first: 'Ella', last: 'Briggs' },
-  // C
-  { first: 'Grace', last: 'Conners' },
-  { first: 'Caleb', last: 'Coleman' },
-  { first: 'Nina', last: 'Clark' },
-  { first: 'Aaron', last: 'Carter' },
-  { first: 'Jade', last: 'Caldwell' },
-  // D
-  { first: 'Ryan', last: 'Dalton' },
-  { first: 'Eva', last: 'Dunn' },
-  { first: 'Leo', last: 'Daniels' },
-  { first: 'Maya', last: 'Dorsey' },
-  { first: 'Ian', last: 'Dillard' },
-  // E
-  { first: 'Sienna', last: 'Ellis' },
-  { first: 'Gavin', last: 'Edwards' },
-  { first: 'Tessa', last: 'Emerson' },
-  { first: 'Asher', last: 'Elliott' },
-  { first: 'Hazel', last: 'Evans' },
-  // G
-  { first: 'Julian', last: 'Gaines' },
-  { first: 'Mila', last: 'Gibson' },
-  { first: 'Axel', last: 'Griffin' },
-  { first: 'Aria', last: 'Garner' },
-  { first: 'Wesley', last: 'George' },
-  // H
-  { first: 'Luna', last: 'Harper' },
-  { first: 'Carson', last: 'Hayes' },
-  { first: 'Bella', last: 'Hoffman' },
-  { first: 'Jonah', last: 'Hammond' },
-  { first: 'Sadie', last: 'Hicks' },
-  // J
-  { first: 'Piper', last: 'Jordan' },
-  { first: 'Jaxon', last: 'Jennings' },
-  { first: 'Aurora', last: 'Justice' },
-  { first: 'Emmett', last: 'Jacobs' },
-  { first: 'Anna', last: 'Jarvis' },
-  // K
-  { first: 'Reid', last: 'Keller' },
-  { first: 'Ruby', last: 'Klein' },
-  { first: 'Tristan', last: 'Kim' },
-  { first: 'Isla', last: 'Kirk' },
-  { first: 'Brody', last: 'Kane' },
-  // L
-  { first: 'Adeline', last: 'Lewis' },
-  { first: 'Elliot', last: 'Lee' },
-  { first: 'Alice', last: 'Lawson' },
-  { first: 'Maddox', last: 'Lowry' },
-  { first: 'Clara', last: 'Leon' },
-  // M
-  { first: 'Juliette', last: 'Murray' },
-  { first: 'Preston', last: 'Moss' },
-  { first: 'Hope', last: 'Moody' },
-  { first: 'Silas', last: 'Meadows' },
-  { first: 'Freya', last: 'Marin' },
-  // R
-  { first: 'Adrian', last: 'Reed' },
-  { first: 'Cora', last: 'Rivers' },
-  { first: 'Jude', last: 'Russell' },
-  { first: 'Vivian', last: 'Roach' },
-  { first: 'Beau', last: 'Ramos' },
-  // S
-  { first: 'Ivy', last: 'Sharp' },
-  { first: 'Damian', last: 'Simmons' },
-  { first: 'Lena', last: 'Stone' },
-  { first: 'Zion', last: 'Strickland' },
-  { first: 'Willa', last: 'Sloan' },
-  // T
-  { first: 'Giselle', last: 'Tanner' },
-  { first: 'Keegan', last: 'Thorne' },
-  { first: 'Skye', last: 'Turner' },
-  { first: 'Finn', last: 'Talbot' },
-  { first: 'Delilah', last: 'Trevino' },
-  // W
-  { first: 'Henry', last: 'Wagner' },
-  { first: 'Eli', last: 'Wallace' },
-  { first: 'Camille', last: 'Wiley' },
-  { first: 'Dean', last: 'Watkins' },
-  { first: 'Faith', last: 'West' },
-];
 
 const groupBy = (array, getKey) => {
   return array.reduce((result, item) => {
@@ -112,10 +14,25 @@ const groupBy = (array, getKey) => {
 };
 
 const ContactList = () => {
+  const [contacts, setContacts] = useState([]);
   const [selectedIndex, setSelectedIndex] = useState(null);
 
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const response = await fetch('api/contacts'); //TODO: connect backend
+        const data = await response.json();
+        setContacts(data);
+      } catch (err) {
+        console.error('Failed to load conatacts:', err);
+      }
+    };
+
+    fetchContacts();
+  }, []);
+
   const groupedContacts = groupBy(contacts, (person) =>
-    person.last[0].toUpperCase()
+    person.last_name[0].toUpperCase()
   );
 
   const sortedKeys = Object.keys(groupedContacts).sort();
@@ -133,16 +50,15 @@ const ContactList = () => {
               const index = itemCounter++;
               return (
                 <div
-                  key={`${person.first}-${person.last}`}
+                  key={`${person.first_name}-${person.last_name}`}
                   className={`person ${selectedIndex === index ? 'selected' : ''}`}
                   onClick={() => {
                     setSelectedIndex(index);
-                    console.log(`Selected: ${person.first}`);
                   }}
                 >
                   <p className="personname">
-                    {person.first}{' '}
-                    <span className="lastname">{person.last}</span>
+                    {person.first_name}{' '}
+                    <span className="lastname">{person.last_name}</span>
                   </p>
                 </div>
               );
